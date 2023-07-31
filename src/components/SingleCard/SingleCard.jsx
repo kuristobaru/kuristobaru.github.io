1443ae5a28e68196cc90f784c1e85d9ddfcbfab5
kuristobaru <cristobal.delgado.esp@gmail.com>
2023-07-31 04:42:40 -0400

    changing src backcover

--- src/components/SingleCard/SingleCard.jsx
+++ src/components/SingleCard/SingleCard.jsx
@@ -20,17 +20,8 @@ export const SingleCard = ({card, handleChoice, flipped, disabled}) => {
           alt='Card is actually in front position'
         />
         :
-        // <div 
-        //   className='bg-purple-700 rounded-2xl 
-        //               border-2 
-        //             border-white ' 
-        //   style={{objectFit:'cover'}}
-        //   onClick={handleClick}
-        //   alt='Card is actually in back position'>
-        //   </div>
-
         <img 
-          src='../public/img/cover.png' 
+          src='../dist/img/cover.png' 
           className='rounded-2xl 
                     border-2 
                     border-white' 
