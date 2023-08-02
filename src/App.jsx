6b1166c338975542ad34ec1ce4d41cdbc5af69a0
kuristobaru <cristobal.delgado.esp@gmail.com>
2023-08-02 00:37:25 -0400

    a

--- src/App.jsx
+++ src/App.jsx
@@ -119,7 +119,7 @@ function App() {
       {/* global scope */}
       <div className='grid grid-cols-12 p-10'>
         <audio controls autoplay className='col-span-2 col-start-6'>
-          <source src="/main/src/assets/soundmemorize.mp3" type="audio/mpeg" />
+          <source src="https://github.com/kuristobaru/kuristobaru.github.io/blob/49ecab61f056846463d5fdc43a462f01a71baaa1/src/assets/soundmemorize.mp3" type="audio/mpeg" />
           Tu navegador no soporta el elemento de audio.
         </audio>
       </div>
