9421289aa3115f2130abf6ae8da8fd9eae18a6d7
kuristobaru <cristobal.delgado.esp@gmail.com>
2023-08-03 00:11:49 -0400

    fixing up responsive in menu

--- src/App.jsx
+++ src/App.jsx
@@ -50,17 +50,18 @@ function App() {
             </div>
             <div>
             </div>
-            <div className="text-6xl font-bold text-white text-center">
+            <div className="text-6xl font-bold text-white text-center grid place-content-center">
               <div className=''>
                 Memorize Modyo
               </div>
             </div>
             <div>
             </div>
-            <div className='text-center'>
+            <div className='text-center grid place-content-center'>
               <button onClick={() => handlePlayBtn()} className='text-4xl 
                 font-bold
                 bg-transparent
+                pl-20 pr-20 pt-2 pb-2
                 animate-pulse
                 hover:bg-violet-700 transition ease-in-out duration-300
                 text-white 
@@ -68,57 +69,60 @@ function App() {
                 border-2 
                 rounded-full
                 border-sky-500 
-                px-10'
+                '
               >
                 Play!
               </button>
             </div>
-            <div className='text-center'>
-              <div className="text-3xl font-bold text-white text-center mt-10">
+            <div className='grid place-content-center'>
+              <div className="text-3xl font-bold text-white text-center mt-10 min-[320px]:col-span-6 min-[320px]:col-start-2">
                 Difficulty
               </div>
             </div>
           </div>
-          <div className='grid grid-rows-3 mt-5'>
-            <div className={(difficulty === 'easy' ? 'text-violet-600' : 'text-white')+' text-2xl text-center animate-pulse'}>
-              <button onClick={() => setDifficulty('easy')} className='hover:text-violet-600 transition ease-in-out duration-500'>
-                Easy
-              </button>
-            </div>
-            <div className={(difficulty==='medium' ? 'text-violet-600' : 'text-white')+' text-2xl text-center animate-pulse'}>
-              <button onClick={() => setDifficulty('medium')} className='hover:text-violet-600 transition ease-in-out duration-500'>
-                Medium
-              </button>
-            </div>
-            <div className={(difficulty==='hard' ? 'text-violet-600' : 'text-white')+' text-2xl text-center animate-pulse'}>
-              <button onClick={() => setDifficulty('hard')} className='hover:text-violet-600 transition ease-in-out duration-500'>
-                Hard
-              </button>
+          <div className='grid place-content-center'>
+            <div className='grid grid-rows-3 mt-5 min-[320px]:col-span-6 min-[320px]:col-start-2'>
+              <div className={(difficulty === 'easy' ? 'text-violet-600' : 'text-white')+' text-2xl text-center animate-pulse'}>
+                <button onClick={() => setDifficulty('easy')} className='hover:text-violet-600 transition ease-in-out duration-500'>
+                  Easy
+                </button>
+              </div>
+              <div className={(difficulty==='medium' ? 'text-violet-600' : 'text-white')+' text-2xl text-center animate-pulse'}>
+                <button onClick={() => setDifficulty('medium')} className='hover:text-violet-600 transition ease-in-out duration-500'>
+                  Medium
+                </button>
+              </div>
+              <div className={(difficulty==='hard' ? 'text-violet-600' : 'text-white')+' text-2xl text-center animate-pulse'}>
+                <button onClick={() => setDifficulty('hard')} className='hover:text-violet-600 transition ease-in-out duration-500'>
+                  Hard
+                </button>
+              </div>
             </div>
           </div>
           <div className='grid grid-rows-2 mt-10'>
-            <div className='grid grid-cols-8'>
-              <input className="col-span-2 col-start-4 p-3 rounded-2xl" 
+            <div className='grid place-content-center'>
+                {needName &&
+                  <div className='col-span-2 col-start-4 text-white text-center'>
+                    You must enter your name first!
+                  </div>
+                }
+            </div>
+            <div className='grid place-content-center'>
+              <input className="pl-20 pr-20 pt-2 pb-2 rounded-2xl" 
                     placeholder='Please enter your name here' 
                     onChange={(e) => setName(e.target.value)} 
                     value={name}
               />
             </div>
-            <div className='grid grid-cols-8'>
-              {needName &&
-                <div className='col-span-2 col-start-4 text-white text-center mt-5'>
-                  You must enter your name first!
-                </div>
-              }
-            </div>
+            
           </div>
           
         </div>
       }
 
       {/* global scope */}
-      <div className='grid grid-cols-12 p-10'>
-        <audio controls loop autoplay className='col-span-2 col-start-6'>
+      <div className='grid place-content-center'>
+        <audio controls loop autoplay className='mt-10'>
           <source src="https://raw.githubusercontent.com/kuristobaru/kuristobaru.github.io/main/src/assets/soundmemorize.mp3" type="audio/mpeg" />
           Tu navegador no soporta el elemento de audio.
         </audio>
